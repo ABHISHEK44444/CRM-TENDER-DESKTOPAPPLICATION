a34d5623463ea8a858e9a26d547edc60347ba518
ABHISHEK44444 <153176102+ABHISHEK44444@users.noreply.github.com>
2025-09-24 16:20:48 +0530

    Update data.js

--- backend/routes/data.js
+++ backend/routes/data.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getDepartments, getDesignations, getTemplates, saveTemplate, deleteTemplate } = require('../controllers/dataController');
+const { getDepartments, getDesignations, getTemplates, saveTemplate, deleteTemplate, getStandardProcessState, updateStandardProcessState } = require('../controllers/dataController');
 
 router.get('/departments', getDepartments);
 router.get('/designations', getDesignations);
@@ -8,5 +8,7 @@ router.get('/templates', getTemplates);
 router.post('/templates', saveTemplate);
 router.put('/templates/:id', saveTemplate);
 router.delete('/templates/:id', deleteTemplate);
+router.get('/processes', getStandardProcessState);
+router.put('/processes', updateStandardProcessState);
 
 module.exports = router;
